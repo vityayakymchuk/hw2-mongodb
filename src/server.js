f6bd5e7604e6fb8cf8d59910d4687368cc93050b
vityayakymchuk <147533752+vityayakymchuk@users.noreply.github.com>
2024-07-18 13:37:20 +0200

    2

--- src/server.js
+++ src/server.js
@@ -11,28 +11,12 @@ const app = express();
 
 const PORT = Number(env('PORT', '3000'));
 
-app.use(
-  pino({
-    transport: {
-      target: 'pino-pretty',
-    },
-  }),
-);
-
-app.use(cors());
-
-
-app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-});
-
 app.get('/contacts', async (req, res) => {
     const contacts = await getAllContacts();
     res.status(200).json({
-        data: contacts,
+	status: 200,
+	message: "Successfully found contacts!",
+	data: contacts,
     });
 });
 
@@ -49,11 +33,21 @@ app.get('/contacts/:contactId', async (req, res) => {
 
     res.status(200).json({
 	status: 200,
-	message: "Successfully found contact with id {**contactId**}!",
+	message: `Successfully found contact with id ${contactId}!`,
 	data: contact,
     });
 
 });
+
+app.use(
+  pino({
+    transport: {
+      target: 'pino-pretty',
+    },
+  }),
+);
+
+app.use(cors());
 
 app.use('*', (req, res, next) => {
   res.status(404).json({
@@ -61,10 +55,15 @@ app.use('*', (req, res, next) => {
 });
 });
 
+app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
 };
-
-// mongodb+srv://vityayakym4uk:sdrVDpspbocTT8Vk@cluster0.194uoho.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0

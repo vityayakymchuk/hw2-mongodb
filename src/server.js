44648aac4ae0710665f6e85419415e604120bb3c
vityayakymchuk <147533752+vityayakymchuk@users.noreply.github.com>
2024-08-17 15:45:57 +0200

    last

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import router from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { UPLOAD_DIR } from './constants/constans.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 export function setupServer() {
 
@@ -27,6 +28,7 @@ app.use(cors());
 app.use(router);
 
 app.use('/uploads', express.static(UPLOAD_DIR));
+app.use('/api-docs', swaggerDocs());
 
 app.use('*', notFoundHandler);
 app.use(errorHandler);
